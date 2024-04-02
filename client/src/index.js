f8da213aeeeae21dc2da748936578e24294352f1
dattali18 <74492594+dattali18@users.noreply.github.com>
2024-04-02 13:43:49 +0300

    fixed error and linked index.js with login.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,30 @@
+import { handleSubmitLogin } from "./pages/login.js";
 // this is the entry point of the application
 
-function navigateTo(pageId) {
+function renderPage(pageId) {
   const pages = ["#login", "#signup", "#home"];
   pages.forEach((page) => {
     const pageElement = document.querySelector(page);
+
     if (page === pageId) {
       pageElement.style.display = "block";
     } else {
       pageElement.style.display = "none";
     }
   });
+}
+
+function navigateTo(pageId) {
+  renderPage(pageId);
+
+  if (pageId === "#login") {
+    // Call functions from login.js
+    handleSubmitLogin();
+  } else if (pageId === "#signup") {
+    // Call functions from signup.js
+  } else if (pageId === "#home") {
+    // Call functions from home.js
+  }
 }
 
 document.getElementById("loginLink").addEventListener("click", () => {
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,24 +1,27 @@
 // this file will handle the logic of the login page in our application
 
 // add event listener to the form submit event
-const loginForm = document.getElementById("login-form");
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault(); // prevent default form submission behavior
+export function handleSubmitLogin() {
+  const loginForm = document.getElementById("login-form");
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent default form submission behavior
 
-  // get the values from the form
-  const name = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const email = document.getElementById("email").value;
+    // get the values from the form
+    const name = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
 
-  // create an object to store the form data
-  const formData = {
-    name,
-    password,
-    email,
-  };
+    // create an object to store the form data
+    const formData = {
+      name,
+      password,
+      email,
+    };
 
-  // convert the form data object to JSON
-  const jsonData = JSON.stringify(formData);
-
-  // TODO: send the form data to the backend
-});
+    // convert the form data object to JSON
+    const jsonData = JSON.stringify(formData);
+    // store jsonData in the local storage
+    
+    // TODO: send the form data to the backend
+  });
+}
