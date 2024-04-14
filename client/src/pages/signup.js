f5194defb2b2b2b750fb1896e7d907c11ff13e72
dattali18 <74492594+dattali18@users.noreply.github.com>
2024-04-14 20:10:38 +0300

    fixed the html

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -5,8 +5,8 @@ import { activateLink, renderPage } from "../utils/navigation.js";
 // Get a reference to the sign-in form element
 let submitForm; // this will be the function called when the form is submitted
 
-const signUpForm = document.getElementById("signup-form");
-signUpForm.addEventListener("submit", (event) => submitForm(event));
+const form = document.getElementById("signup-form");
+form.addEventListener("submit", (event) => submitForm(event));
 
 export function renderSignUpPage() {
   // Add an event listener to the form's submit event
@@ -15,7 +15,7 @@ export function renderSignUpPage() {
 
     const formData = new FormData(form);
     const contact = {
-      name: formData.get("sing-name"),
+      name: formData.get("sign-name"),
       password: formData.get("sign-password"),
       email: formData.get("sign-email"),
       confirmPassword: formData.get("sign-confirm-password"),
